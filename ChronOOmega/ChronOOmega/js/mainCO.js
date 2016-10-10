3bae4abbee81018c65c90274818fb1ea3aea2e6f
Omar Tiznado <omar.tiznado@gmail.com>
2016-10-09 23:30:28 -0500

    Actualización a nuevo tipo de menu paralax

---
+++ ChronOOmega/ChronOOmega/js/mainCO.js
@@ -0,0 +1,10 @@
+/**
+ * Author: J. Omar Tiznado C. (Chrono)
+ * Date: 21/08/2016
+ * CSS Chrono Omega Portal Style for home page
+ */
+
+function hidde(elementName){
+	//document.getElementById(elementName);
+	window.alert('HOLA: ' + elementName);
+}
---
+++ ChronOOmega/ChronOOmega/js/menu.js
@@ -0,0 +1,59 @@
+/**
+ * Author: J. Omar Tiznado C. (Chrono)
+ * Date: 09/10/2016
+ * JS Chrono Omega Portal Scrypts for main menu
+ */
+//Based on the Scroller function from @sallar
+
+function Scroller()
+{
+  this.latestKnownScrollY = 0;
+  this.ticking            = false;
+}
+
+Scroller.prototype = {
+ 
+  init: function() {
+    window.addEventListener('scroll', this.onScroll.bind(this), false);
+    $blur.css('background-image',$('header:first-of-type').css('background-image'));
+  },
+
+
+  onScroll: function() {
+    this.latestKnownScrollY = window.scrollY;
+    this.requestTick();
+  },
+
+  
+  requestTick: function() {
+    if( !this.ticking ) {
+      window.requestAnimFrame(this.update.bind(this));
+    }
+    this.ticking = true;
+  },
+
+  update: function() {
+    var currentScrollY = this.latestKnownScrollY;
+    this.ticking       = false;
+    
+    
+    var slowScroll = currentScrollY / 2
+      , blurScroll = currentScrollY * 2
+      , opaScroll = 1.4 - currentScrollY / 400;
+   if(currentScrollY > wHeight)
+     $('nav').css('position','fixed');
+   else
+     $('nav').css('position','absolute');
+    
+    $content.css({
+      'transform'         : 'translateY(' + slowScroll + 'px)',
+      '-moz-transform'    : 'translateY(' + slowScroll + 'px)',
+      '-webkit-transform' : 'translateY(' + slowScroll + 'px)',
+      'opacity' : opaScroll
+    });
+    
+    $blur.css({
+      'opacity' : blurScroll / wHeight
+    });
+  }
+};
